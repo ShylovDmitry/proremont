c8bff52761a2442b6d94164fbd8832411d216b55
Shylov Dmytro <dshylov@n-ix.com>
2017-09-13 00:10:23 +0300

    Corrections

--- public/wp-content/themes/common/template-catalog_master.php
+++ public/wp-content/themes/common/template-catalog_master.php
@@ -5,40 +5,54 @@
 <?php get_template_part('breadcrumb'); ?>
 
 <div class="container colored-box py-3">
-    <?php $catalog_master_page = get_page_by_template_name('template-catalog_master.php'); ?>
     <div class="row">
-        <div class="col-12">
-            <h3 class="mb-4"><?php echo get_the_title($catalog_master_page) ?></h3>
-        </div>
-    <?php
-    $main_catalogs = get_terms(array(
-        'parent' => 0,
-        'hierarchical' => false,
-        'taxonomy' => 'catalog_master',
-        'hide_empty' => false,
-    ));
-    ?>
-    <?php foreach ($main_catalogs as $main_catalog): ?>
-        <div class="col-12">
-            <hr />
-            <h6><a href="<?php echo esc_url( get_term_link($main_catalog) ); ?>"><?php echo $main_catalog->name; ?></a> (<?php echo $main_catalog->count; ?>)</h6>
-
-            <?php
-            $sub_catalogs = get_terms(array(
-                'parent' => $main_catalog->term_id,
-                'hierarchical' => false,
-                'taxonomy' => 'catalog_master',
-                'hide_empty' => false,
-            ));
-            ?>
+        <div class="col">
             <div class="row">
-            <?php foreach ($sub_catalogs as $sub_pos => $sub_catalog): ?>
-                <div class="col-4"><a href="<?php echo esc_url( get_term_link($sub_catalog) ); ?>"><?php echo $sub_catalog->name; ?></a> (<?php echo $sub_catalog->count; ?>)</div>
-                <?php if (($sub_pos+1) % 3 == 0): ?><div class="w-100"></div><?php endif; ?>
-            <?php endforeach; ?>
+                <div class="col-12">
+                    <?php $section = pror_get_section(); ?>
+                    <h3 class="mb-4"><?php the_title() ?> - <?php echo $section->name; ?></h3>
+                </div>
+
+                <?php
+                $main_catalogs = get_terms(array(
+                    'parent' => 0,
+                    'hierarchical' => false,
+                    'taxonomy' => 'catalog_master',
+                    'hide_empty' => false,
+                ));
+                ?>
+                <?php foreach ($main_catalogs as $main_catalog): ?>
+                    <div class="col-12">
+                        <hr />
+                        <h6><a href="<?php echo esc_url( get_term_link($main_catalog) ); ?>"><?php echo $main_catalog->name; ?></a></h6>
+
+                        <?php
+                        $sub_catalogs = get_terms(array(
+                            'parent' => $main_catalog->term_id,
+                            'hierarchical' => false,
+                            'taxonomy' => 'catalog_master',
+                            'hide_empty' => false,
+                        ));
+                        ?>
+                        <div class="row">
+                            <?php $halved = array_chunk($sub_catalogs, ceil(count($sub_catalogs)/2));?>
+                            <?php foreach ($halved as $half): ?>
+                                <div class="col-6">
+                                    <?php foreach ($half as $sub_catalog): ?>
+                                        <div>
+                                            <a href="<?php echo esc_url( get_term_link($sub_catalog) ); ?>"><?php echo $sub_catalog->name; ?></a>
+                                        </div>
+                                    <?php endforeach; ?>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+
             </div>
         </div>
-    <?php endforeach; ?>
+
+        <?php get_sidebar('banner-right'); ?>
     </div>
 </div>
 
