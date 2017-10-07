021893073b0cff8934c8ae104a99df5cbab29e01
Shylov Dmytro <dshylov@n-ix.com>
2017-10-07 03:13:58 +0300

    Visual breadcrumbs

--- public/wp-content/themes/common/modules/breadcrumbs/front/front.php
+++ public/wp-content/themes/common/modules/breadcrumbs/front/front.php
@@ -1,5 +1,9 @@
 <?php
 
+add_action('wp_print_styles', function () {
+    wp_enqueue_style('breadcrumbs-common', get_module_css('breadcrumbs/common.css'), array(), dlv_get_ver());
+});
+
 add_filter('wpseo_breadcrumb_links', function($crumbs) {
     $section = pror_get_section();
     if ($section && $section->slug) {
