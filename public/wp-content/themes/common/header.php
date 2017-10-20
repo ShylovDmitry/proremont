ee110fe3bfa4b09d7edb913aea4700a200b9c25a
Shylov Dmytro <dshylov@n-ix.com>
2017-10-20 12:47:16 +0300

    Added favicon

--- public/wp-content/themes/common/header.php
+++ public/wp-content/themes/common/header.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php wp_title('&ndash;', true, 'right'); ?></title>
 
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php module_img('theme/favicon/apple-touch-icon.png'); ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php module_img('theme/favicon/favicon-32x32.png'); ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php module_img('theme/favicon/favicon-16x16.png'); ?>">
+    <link rel="manifest" href="<?php module_img('theme/favicon/manifest.json'); ?>">
+    <link rel="mask-icon" href="<?php module_img('theme/favicon/safari-pinned-tab.svg'); ?>" color="#5bbad5">
+    <link rel="shortcut icon" href="<?php module_img('theme/favicon/favicon.ico'); ?>">
+    <meta name="apple-mobile-web-app-title" content="ProRemont">
+    <meta name="application-name" content="ProRemont">
+    <meta name="theme-color" content="#ffffff">
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
