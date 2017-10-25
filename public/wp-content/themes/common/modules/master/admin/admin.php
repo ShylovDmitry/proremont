bda66b180521bb49bd8bab344f58438569f467df
Shylov Dmytro <dshylov@n-ix.com>
2017-10-25 15:30:48 +0300

    Corrections

--- public/wp-content/themes/common/modules/master/admin/admin.php
+++ public/wp-content/themes/common/modules/master/admin/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 add_action('admin_enqueue_scripts', function($hook) {
-    if (pror_current_user_has_role('master') && 'profile.php' == $hook) {
+    if (pror_current_user_has_role('master') && is_admin()) {
         wp_enqueue_style('admin-profile', get_module_css('master/admin-profile.css'), array(), dlv_get_ver());
         wp_enqueue_script('admin-profile', get_module_js('master/admin-profile.js'), array(), dlv_get_ver(), true);
     }
