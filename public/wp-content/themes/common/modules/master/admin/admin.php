fa8756c101127edb10c527b6cfd47dbdf4d38f4f
Shylov Dmytro <dshylov@n-ix.com>
2017-10-25 15:46:53 +0300

    Display only master's images

--- public/wp-content/themes/common/modules/master/admin/admin.php
+++ public/wp-content/themes/common/modules/master/admin/admin.php
@@ -6,3 +6,24 @@ add_action('admin_enqueue_scripts', function($hook) {
         wp_enqueue_script('admin-profile', get_module_js('master/admin-profile.js'), array(), dlv_get_ver(), true);
     }
 });
+
+
+add_action('pre_get_posts', function($wp_query_obj) {
+    global $current_user, $pagenow;
+
+    $is_attachment_request = ($wp_query_obj->get('post_type')=='attachment');
+
+    if( !$is_attachment_request )
+        return;
+
+    if( !is_a( $current_user, 'WP_User') )
+        return;
+
+    if( !in_array( $pagenow, array( 'upload.php', 'admin-ajax.php' ) ) )
+        return;
+
+    if( !current_user_can('delete_pages') )
+        $wp_query_obj->set('author', $current_user->ID );
+
+    return;
+});
