6502ed91371beb31cb3ca1b42ea29da8122eadac
Shylov Dmytro <dshylov@n-ix.com>
2017-11-17 11:44:07 +0200

    Added pixel, fixed ga

--- public/wp-content/themes/common/modules/facebook/front/front.php
+++ public/wp-content/themes/common/modules/facebook/front/front.php
@@ -1,6 +1,12 @@
 <?php
 
-add_filter('wp_head', function() {
+add_action('wp_head', function() {
+    if (!WP_ENV_LOCAL) {
+        module_template('facebook/pixel');
+    }
+});
+
+add_action('login_head', function() {
     if (!WP_ENV_LOCAL) {
         module_template('facebook/pixel');
     }
--- public/wp-content/themes/common/modules/google/front/front.php
+++ public/wp-content/themes/common/modules/google/front/front.php
@@ -1,6 +1,14 @@
 <?php
 
-add_filter('wp_head', function() {
+add_action('wp_head', function() {
+    if (!WP_ENV_LOCAL) {
+        module_template('google/ga');
+        module_template('google/gtm');
+        module_template('google/adsense');
+    }
+});
+
+add_action('login_head', function() {
     if (!WP_ENV_LOCAL) {
         module_template('google/ga');
         module_template('google/gtm');
