0a4da2da37262999a5a5de3e35ede9338e3504f5
Shylov Dmytro <dshylov@n-ix.com>
2017-12-13 11:10:52 +0200

    Added link

--- public/wp-content/themes/common/modules/master/template/item.php
+++ public/wp-content/themes/common/modules/master/template/item.php
@@ -31,5 +31,9 @@
     </div>
     <div class="excerpt">
         <?php the_excerpt(); ?>
+
+        <div class="more">
+            <a href="<?php echo esc_url( get_permalink() ); ?>">Переглянути &raquo;</a>
+        </div>
     </div>
 </div>
