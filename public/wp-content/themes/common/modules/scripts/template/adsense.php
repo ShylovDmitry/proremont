ed4ea1a194b3b015bfcc1faee82e3609fcfe9a7c
Shylov Dmytro <dshylov@n-ix.com>
2018-01-22 14:44:14 +0200

    Updated adsense

--- public/wp-content/themes/common/modules/scripts/template/adsense.php
+++ public/wp-content/themes/common/modules/scripts/template/adsense.php
@@ -1,7 +1,7 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-5573040985659748",
+    google_ad_client: "ca-pub-9097779744689387",
     enable_page_level_ads: true
   });
 </script>
