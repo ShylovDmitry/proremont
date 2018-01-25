e7203f78f2cd73b63b40a113b6624e0220a7e9c2
Shylov Dmytro <dshylov@n-ix.com>
2018-01-25 04:45:33 +0200

    Updated comopser

--- public/wp-content/wp-rocket-config/proremont.local.php
+++ public/wp-content/wp-rocket-config/proremont.local.php
@@ -3,15 +3,13 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 $rocket_cookie_hash = '65bf72a67b8e7dec55704c26c3ca09a9';
 $rocket_secret_cache_key = '59dcd55504c26915395316';
-$rocket_minify_css_legacy = '0';
-$rocket_minify_js_legacy = '0';
 $rocket_cache_mobile = '1';
-$rocket_cache_reject_uri = '/|/blog/%d0%bf%d0%be%d1%81%d1%82%d1%80%d0%be%d0%b9-%d1%81%d0%b2%d0%be%d1%8e-%d0%bc%d0%b5%d1%87%d1%82%d1%83-%d1%81%d0%ba%d0%b0%d1%87%d0%b0%d1%82%d1%8c-%d0%b1%d0%b5%d1%81%d0%bf%d0%bb%d0%b0%d1%82%d0%bd%d0%be/|(.*)/feed/?|/wp-json/(.*)';
+$rocket_cache_ssl = '1';
+$rocket_cache_reject_uri = '/|(.*)/feed/?|/wp-json/(.*)';
 $rocket_cache_reject_cookies = 'wordpress_logged_in_|wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
 $rocket_cache_reject_ua = 'facebookexternalhit';
 $rocket_cache_query_strings = array (
 );
-$rocket_cache_ssl = '1';
 $rocket_cache_mandatory_cookies = '';
 $rocket_cache_dynamic_cookies = array (
 );
