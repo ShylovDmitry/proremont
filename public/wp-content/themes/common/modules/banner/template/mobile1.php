d0f2b009451c5e0f00dc451f4617d44a4212c7ef
Shylov Dmytro <dshylov@n-ix.com>
2018-01-30 00:40:53 +0200

    Fixed mobile ad styles

--- public/wp-content/themes/common/modules/banner/template/mobile1.php
+++ public/wp-content/themes/common/modules/banner/template/mobile1.php
@@ -1,7 +1,7 @@
 <div id="banner_mobile1">
 
     <!-- /21681373772/mobile1 -->
-    <div id='div-mobile1' style='height:300px; width:250px;'>
+    <div id='div-mobile1' style='height:250px; width:300px;'>
         <script>
             googletag.cmd.push(function() { googletag.display('div-mobile1'); });
         </script>
