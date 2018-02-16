cd61c98ac9ae73940239a1a7e2c816c9c0510732
Shylov Dmytro <dshylov@n-ix.com>
2018-02-16 13:12:24 +0200

    Added help function

--- public/wp-content/themes/common/modules/theme/front/help.php
+++ public/wp-content/themes/common/modules/theme/front/help.php
@@ -92,3 +92,66 @@
 //        exit;
 //    }
 //}, 11);
+
+//add_action('init', function() {
+//    if (isset($_GET['pror_offset'])) {
+//
+//        $section = get_term_by('slug', 'chernovtsy', 'section');
+//        $locations = get_field('locations', 'section_' . $section->term_id);
+//
+//
+//        $pp = new WP_Query([
+//            'post_type' => 'master',
+//            'post_status' => 'any',
+//            'posts_per_page' => isset($_GET['pror_limit']) ? $_GET['pror_limit'] : 10000,
+//            'offset' => $_GET['pror_offset'],
+//            'orderby' => 'ID',
+//            'tax_query' => array(
+//                array(
+//                    'taxonomy' => 'location',
+//                    'terms' => $locations,
+//                    'include_children' => false,
+//                    'operator' => 'IN',
+//                ),
+//            ),
+//        ]);
+//
+//        echo '<table>';
+//        echo '<tr>
+//            <th>ID</th>
+//            <th>user_login</th>
+//            <th>Title</th>
+//            <th>Phone</th>
+//            <th>Admin Area</th>
+//            <th>View Page</th>
+//            <th>Comment</th>
+//        </tr>';
+//
+//        $c = 0;
+//        while ($pp->have_posts()) {
+//            $c++;
+//            $pp->the_post();
+//
+//            global $post;
+//            $user_id = $post->post_author;
+//            $user = get_user_by('ID', $user_id);
+//            $master_post_id = get_the_ID();
+//
+//            echo '<tr>
+//                <td>' . $user_id . '</td>
+//                <td>' . $user->user_login . '</td>
+//                <td>' . get_field('master_title', 'user_'.$user_id) . '</td>
+//                <td>' . implode('<br/>', array_map(function($value) { return $value['text']; }, pror_get_master_phones($user_id))) . '</td>
+//                <td>' . str_replace('http://proremont.local', 'https://proremont.co', get_edit_user_link($post->post_author)) . '</td>
+//                <td>' . str_replace('http://proremont.local', 'https://proremont.co', get_the_permalink($master_post_id)) . '</td>
+//                <td></td>
+//            </tr>';
+//
+//
+//        }
+//
+//        echo '</table>';
+//
+//        exit;
+//    }
+//}, 11);
