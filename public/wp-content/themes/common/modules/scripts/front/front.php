159275361e2f772d9077a8dc2bf68f2723229d90
Shylov Dmytro <dshylov@n-ix.com>
2018-02-18 16:25:04 +0200

    Added stats to admin area

--- public/wp-content/themes/common/modules/scripts/front/front.php
+++ public/wp-content/themes/common/modules/scripts/front/front.php
@@ -18,6 +18,15 @@ add_action('login_head', function() {
     }
 });
 
+add_action('admin_head', function() {
+    if (!WP_ENV_LOCAL && is_user_logged_in() && (pror_current_user_has_role('master') || pror_current_user_has_role('subscriber'))) {
+        module_template('scripts/ga');
+        module_template('scripts/gtm');
+        module_template('scripts/pixel');
+        module_template('scripts/yandex_metrica');
+    }
+});
+
 add_action('pror_body_before', function() {
     module_template('scripts/gtm_body');
 });
