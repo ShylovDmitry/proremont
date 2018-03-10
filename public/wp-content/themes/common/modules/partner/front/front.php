8c15134dc2b5c40068303061e6205a7f2e23ae8b
Shylov Dmytro <dshylov@n-ix.com>
2018-03-10 04:48:11 +0200

    Fixed breadcrumbs for partners

--- public/wp-content/themes/common/modules/partner/front/front.php
+++ public/wp-content/themes/common/modules/partner/front/front.php
@@ -7,3 +7,19 @@ add_action('wp_print_styles', function () {
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('partner-common', get_module_js('partner/common.js'), array('jquery'), dlv_get_ver(), true);
 });
+
+add_filter('wpseo_breadcrumb_links', function($crumbs) {
+    if (get_post_type() == 'partner') {
+        $page = get_page_by_path('partners');
+
+        array_splice($crumbs, 1, 0, array(
+            array(
+                'text' => get_the_title($page),
+                'url' => get_permalink($page),
+                'allow_html' => true,
+            )
+        ));
+    }
+
+    return $crumbs;
+});
