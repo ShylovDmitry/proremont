a7f647c12dd54619b385e6fdb029fe29bc3161c2
Shylov Dmytro <dshylov@n-ix.com>
2018-04-16 23:13:35 +0300

    New email

---
+++ public/emails/master-welcome/a.php
@@ -0,0 +1,21 @@
+<table cellpadding="0" cellspacing="0" border="0" style="background: none; border-width: 0px; border: 0px; margin: 0; padding: 0; width: 100%">
+<tr><td style="background: #424949; text-align: center; padding: 20px 10px; color: #ffffff; font-size: 20px; font-family: Verdana, Arial, Helvetica, sans-serif;">
+        <a href="https://proremont.co/" style="text-decoration: none; font-weight: normal;" target="_blank"><img id="preview-image-url" src="<?php echo sprintf('%s://%s', $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST']); ?>/emails/logo-proremont.png" height="80" style="max-width: 100%; height: auto; max-height: 80px;"/></a>
+        <div style="padding-top: 10px;">Всеукраинский онлайн-каталог мастеров</div>
+</td></tr>
+<tr><td style="text-align: center; padding: 40px 0; color: #000000; font-size: 20px; font-family: Verdana, Arial, Helvetica, sans-serif;">
+        <p style="font-size: 20px; font-weight: bold;">Регистрация прошла успешно!</p>
+        <p style="font-size: 16px;">Установи пароль, что бы войти в свой кабинет.</p>
+        <p><a href="[password_url]" style="color: #E67E22; font-size: 16px;">Установить пароль &raquo;</a></p>
+</td></tr>
+<tr><td style="background: #424949; text-align: center; padding: 20px 0; color: #ffffff; font-size: 20px; font-family: Arial, Helvetica, sans-serif;">
+        <p>
+            <a href="#"><img src="<?php echo sprintf('%s://%s', $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST']); ?>/emails/icon-fb.png" height="36" /></a> &nbsp;
+            <a href="#"><img src="<?php echo sprintf('%s://%s', $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST']); ?>/emails/icon-instagram.png" height="36" /></a> &nbsp;
+            <a href="#"><img src="<?php echo sprintf('%s://%s', $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST']); ?>/emails/icon-gplus.png" height="36" /></a> &nbsp;
+            <a href="#"><img src="<?php echo sprintf('%s://%s', $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST']); ?>/emails/icon-vk.png" height="36" /></a>
+        </p>
+        <p><a href="mailto:info@proremont.co" style="font-size: 16px; color: #B2BABB; text-decoration: none; border-bottom: 1px solid #B2BABB;">info@proremont.co</a></p>
+        <p style="font-size: 14px; color: #B2BABB;">2018 © ProRemont. Все права защищены.</p>
+</td></tr>
+</table>
