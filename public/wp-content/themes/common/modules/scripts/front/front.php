3e91025aee26ba2c9108a278a8a8c10c222a100a
Shylov Dmytro <dshylov@n-ix.com>
2018-04-24 21:14:43 +0300

    Removed GTM

--- public/wp-content/themes/common/modules/scripts/front/front.php
+++ public/wp-content/themes/common/modules/scripts/front/front.php
@@ -3,7 +3,7 @@
 add_action('wp_head', function() {
     if (!WP_ENV_LOCAL) {
         module_template('scripts/ga');
-        module_template('scripts/gtm');
+//        module_template('scripts/gtm');
         module_template('scripts/pixel');
         module_template('scripts/yandex_metrica');
     }
@@ -12,7 +12,7 @@ add_action('wp_head', function() {
 add_action('login_head', function() {
     if (!WP_ENV_LOCAL) {
         module_template('scripts/ga');
-        module_template('scripts/gtm');
+//        module_template('scripts/gtm');
         module_template('scripts/pixel');
         module_template('scripts/yandex_metrica');
     }
@@ -21,12 +21,12 @@ add_action('login_head', function() {
 add_action('admin_head', function() {
     if (!WP_ENV_LOCAL && is_user_logged_in() && (pror_current_user_has_role('master') || pror_current_user_has_role('subscriber'))) {
         module_template('scripts/ga');
-        module_template('scripts/gtm');
+//        module_template('scripts/gtm');
         module_template('scripts/pixel');
         module_template('scripts/yandex_metrica');
     }
 });
 
 add_action('pror_body_before', function() {
-    module_template('scripts/gtm_body');
+//    module_template('scripts/gtm_body');
 });
