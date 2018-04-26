e7b2f827e8ef5f377cee860dacdc7aba3bffc356
Shylov Dmytro <dshylov@n-ix.com>
2018-04-26 11:43:03 +0300

    Updated GTM

--- public/wp-content/themes/common/modules/scripts/front/front.php
+++ public/wp-content/themes/common/modules/scripts/front/front.php
@@ -1,32 +1,45 @@
 <?php
 
 add_action('wp_head', function() {
-    if (!WP_ENV_LOCAL) {
-        module_template('scripts/ga');
-//        module_template('scripts/gtm');
-        module_template('scripts/pixel');
-        module_template('scripts/yandex_metrica');
+    if (WP_ENV_LOCAL) {
+        return;
     }
+
+//    module_template('scripts/ga');
+    module_template('scripts/gtm');
+    module_template('scripts/pixel');
+    module_template('scripts/yandex_metrica');
 });
 
 add_action('login_head', function() {
-    if (!WP_ENV_LOCAL) {
-        module_template('scripts/ga');
-//        module_template('scripts/gtm');
-        module_template('scripts/pixel');
-        module_template('scripts/yandex_metrica');
+    if (WP_ENV_LOCAL) {
+        return;
     }
+
+//    module_template('scripts/ga');
+    module_template('scripts/gtm');
+    module_template('scripts/pixel');
+    module_template('scripts/yandex_metrica');
 });
 
 add_action('admin_head', function() {
-    if (!WP_ENV_LOCAL && is_user_logged_in() && (pror_current_user_has_role('master') || pror_current_user_has_role('subscriber'))) {
-        module_template('scripts/ga');
-//        module_template('scripts/gtm');
-        module_template('scripts/pixel');
-        module_template('scripts/yandex_metrica');
+    if (WP_ENV_LOCAL) {
+        return;
     }
+    if (is_user_logged_in() && !pror_user_has_role('master subscriber')) {
+        return;
+    }
+
+//    module_template('scripts/ga');
+    module_template('scripts/gtm');
+    module_template('scripts/pixel');
+    module_template('scripts/yandex_metrica');
 });
 
 add_action('pror_body_before', function() {
-//    module_template('scripts/gtm_body');
+    if (WP_ENV_LOCAL) {
+        return;
+    }
+
+    module_template('scripts/gtm_body');
 });
--- public/wp-content/themes/common/modules/scripts/template/gtm_body.php
+++ public/wp-content/themes/common/modules/scripts/template/gtm_body.php
@@ -1,6 +1,4 @@
-<?php if (!WP_ENV_LOCAL) : ?>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KL8XTXD"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-<?php endif; ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K2JCZ57"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
