bbefb3f37c99e7951ad87b439706477ec98fce03
Shylov Dmytro <dshylov@n-ix.com>
2018-06-19 03:48:03 +0300

    Added filters for top masters

--- public/wp-content/plugins/pror_stats/pror_stats.php
+++ public/wp-content/plugins/pror_stats/pror_stats.php
@@ -73,11 +73,50 @@ function pror_stats_get_top_period($type, $period_in_days = 30) {
 
 	$table_name = $wpdb->prefix . 'pror_stats';
 
+	$where_section = '';
+	if ($_GET['section']) {
+	    $locations = get_term_meta($_GET['section'], 'locations');
+	    if ($locations && $locations[0]) {
+	        $locations_str = implode(',', $locations[0]);
+            $where_section = " AND um_section.meta_value IN ({$locations_str})";
+        }
+    }
+
+	$where_catalog = '';
+	if ($_GET['catalog']) {
+        $catalog_ids = [$_GET['catalog']];
+
+	    $main = get_term($_GET['catalog'], 'catalog_master');
+	    if ($main->parent == 0) {
+            $catalog_ids += get_terms([
+                'taxonomy' => 'catalog_master',
+                'hide_empty' => 0,
+                'fields' => 'ids',
+                'parent' => $_GET['catalog'],
+            ]);
+        }
+
+        $catalog_conds = [];
+        foreach ($catalog_ids as $catalog_id) {
+            $catalog_conds[] = "um_catalog.meta_value LIKE '%\"{$catalog_id}\"%'";
+        }
+
+        if ($catalog_conds) {
+            $where_catalog = " AND (" . implode(' OR ', $catalog_conds) . ")";
+        }
+    }
+
+    $time = date_i18n('Y-m-d H:i:s', strtotime("-{$period_in_days} days"), true);
+
+    $sql = "SELECT ps.post_id, COUNT(ps.id) page_view_count FROM $table_name ps
+        LEFT JOIN wp_posts p ON p.ID = ps.post_id
+        LEFT JOIN wp_usermeta um_section ON um_section.user_id = p.post_author AND um_section.meta_key = 'master_location'
+        LEFT JOIN wp_usermeta um_catalog ON um_catalog.user_id = p.post_author AND um_catalog.meta_key = 'master_catalog'
+        WHERE ps.time >= '{$time}' AND ps.name = '{$type}'{$where_section}{$where_catalog}
+        GROUP BY ps.post_id ORDER BY page_view_count DESC LIMIT 100";
+
     return $wpdb->get_results(
-        $wpdb->prepare("SELECT post_id, COUNT(id) page_view_count FROM $table_name WHERE time >= %s AND name = %s GROUP BY post_id ORDER BY page_view_count DESC LIMIT 100", array(
-            date_i18n('Y-m-d H:i:s', strtotime("-{$period_in_days} days"), true),
-            $type
-        ))
+        $sql
     );
 }
 
@@ -91,34 +130,85 @@ function pror_stats_display_admin_page() {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 
-	$period = 30;
+
+	if ($_GET['period'] == '7d') {
+	    $period = 7;
+    } else {
+        $period = 30;
+    }
     $res = pror_stats_get_top_period('master_page_view', $period);
+
+	$section_dropdown = wp_dropdown_categories([
+	    'show_option_all' => 'All',
+	    'echo' => 0,
+	    'hide_empty' => 0,
+        'name' => 'section',
+	    'taxonomy' => 'section',
+        'selected' => $_GET['section'],
+    ]);
+
+	$catalog_dropdown = wp_dropdown_categories([
+	    'show_option_all' => 'All',
+	    'echo' => 0,
+	    'hide_empty' => 0,
+        'hierarchical' => true,
+        'name' => 'catalog',
+	    'taxonomy' => 'catalog_master',
+        'selected' => $_GET['catalog'],
+    ]);
+
+	$period_selected_7d = ($_GET['period'] == '7d') ? ' selected' : '';
+	$period_selected_30d = ($_GET['period'] == '30d') ? ' selected' : '';
 
 	echo <<<"EOF"
 <div class="wrap">
 <h1>Master Top</h1>
+
+<form id="posts-filter" action="" method="get" style="margin-bottom: 5px;">
+    <input type="hidden" name="post_type" value="master" />
+    <input type="hidden" name="page" value="master-top" />
+    
+    {$section_dropdown}
+    {$catalog_dropdown}
+    
+    <select name="period">
+        <option value="30d"{$period_selected_30d}>30 дней</option>
+        <option value="7d" {$period_selected_7d}>7 дней</option>
+    </select>
+    
+    <input type="submit" value="Фильтр" class="button" />
+</form>
+
 <table class="wp-list-table widefat fixed striped posts">
     <thead>
         <tr>
             <td class="manage-column">Name</td>
-            <td class="manage-column">Page View</td>
-            <td class="manage-column">Show Phone</td>
-            <td class="manage-column">Conversion</td>
+            <td class="manage-column" width="100">Page View</td>
+            <td class="manage-column" width="100">Show Phone</td>
+            <td class="manage-column" width="100">Conversion</td>
+            <td class="manage-column" width="30">Edit</td>
         </tr>
     </thead>
     <tbody>
 EOF;
     foreach ($res as $r) {
+        $post = get_post($r->post_id);
+        $is_pro = get_field('master_is_pro', 'user_'.$post->post_author);
+
         $name = get_the_title($r->post_id);
-        $link = get_edit_post_link($r->post_id);
+        $link = get_permalink($r->post_id);
+        $edit_link = get_edit_post_link($r->post_id);
         $show_phone_count = pror_stats_get_period('master_show_phone', $r->post_id, $period);
         $c = round(100*$show_phone_count/$r->page_view_count, 2);
+
+        $pro_str = $is_pro ? "<span style='background:#E67E22; color:#fff; font-weight:900; font-size:75%; padding:2px 4px; border-radius:2px;'>PRO</span> " : '';
         echo <<<"EOF"
             <tr>
-                <td class="manage-column"><a href="{$link}">{$name}</a></td>
+                <td class="manage-column">{$pro_str}<a href="{$link}" target="_blank">{$name}</a></td>
                 <td class="manage-column">{$r->page_view_count}</td>
                 <td class="manage-column">{$show_phone_count}</td>
                 <td class="manage-column">{$c}%</td>
+                <td class="manage-column"><a href="{$edit_link}"><span class="dashicons-before dashicons-admin-generic"></span></a></td>
             </tr>
 EOF;
     }
