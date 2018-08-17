4b5efabd1eb5b4db9b986c1b699da6a14b02761c
Shylov Dmytro <dshylov@n-ix.com>
2018-08-17 11:20:11 +0300

    Fixed lang

--- public/wp-content/themes/common/modules/theme/front/functions.php
+++ public/wp-content/themes/common/modules/theme/front/functions.php
@@ -10,12 +10,6 @@ add_filter('oa_social_login_filter_new_user_role', function($user_role) {
     return $user_role;
 });
 
-add_filter('wp_head', function() {
-    echo '<link rel="alternate" href="';
-    self_link();
-    echo '" hreflang="ru" />';
-});
-
 add_filter('logout_url', function($logouturl, $redir) {
     return $logouturl . '&amp;redirect_to=' . get_permalink();
 }, 10, 2);
