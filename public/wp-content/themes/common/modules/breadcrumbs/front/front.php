c31f77acdbaeae96a3f3e4cecfe34c6c4de0bc39
Shylov Dmitry <shylov.dmitry@gmail.com>
2019-04-12 22:12:45 +0300

    Fixed breadcrumbs (not finished)

--- public/wp-content/themes/common/modules/breadcrumbs/front/front.php
+++ public/wp-content/themes/common/modules/breadcrumbs/front/front.php
@@ -3,3 +3,19 @@
 add_action('wp_print_styles', function () {
     wp_enqueue_style('breadcrumbs-common', get_module_css('breadcrumbs/common.css'), array(), dlv_get_ver());
 });
+
+
+add_filter( 'wpseo_breadcrumb_links', function( $links ) {
+	if (is_singular( 'tender' )) {
+		$post = get_page_by_path('tenders');
+		$lang_post = pll_get_post($post->ID);
+
+		$breadcrumb[] = array(
+			'id' => $lang_post,
+		);
+
+		array_splice( $links, 1, -2, $breadcrumb );
+	}
+
+	return $links;
+});
